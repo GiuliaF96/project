9690fe510c34bd87446bd1e7105aa7a8bff80d0f
Giulia <giuliaferrandina@gmail.com>
2025-12-17 12:17:29 +0100

    print list

--- follower github/main.py
+++ follower github/main.py
@@ -1,15 +1,12 @@
-"""
-
-"""
 
 from requests import get
 import re
 
-BASE_URL: str = "https://github.com/emanuelegurini" 
+BASE_URL: str = "https://github.com" 
 END_URL: str = "tab=followers"
 
 PATTERN = r'<a\s+[^>]*href="https://github\.com/([^/]+)\?page=(\d+)&amp;tab=followers"[^>]*>Next</a>'
-
+PATTERN_USER = r'<span class="Link--secondary(?: pl-1)?">([^<]+)</span>'
 def is_next_button_present(text: str) -> bool:
   if not text:
     raise ValueError("La stringa non puo essere vuota!")
@@ -17,28 +14,65 @@ def is_next_button_present(text: str) -> bool:
   return bool(re.search(PATTERN, text))
 
 def main() -> None:
-  print("Start del programma")
+    counter: int = 0
 
-  controller: bool = True
-  counter: int = 1
+    print("Start del programma")
 
-  while controller:
-    url = f"{BASE_URL}?page={counter}&{END_URL}"
-    try:
-      response = get(url)
+    while True:
+     try: 
 
-      with open(f"tmp/pagina-{counter}.txt", "w") as f:
-        if controller:
-          f.write(response.text)
-          controller = is_next_button_present(response.text)
-          counter = counter + 1 
-          print("File salvato")
+        nome_utente: str = input("Inserisci lo username del profilo gitHub che vuoi analizzare: ")
+        
+        if not nome_utente:
+            raise ValueError("Il nome utente non può essere vuoto")
+        #TODO: il nome utente exit è presente
+        if nome_utente.strip().lower() == "exit":
+            break
+        
+        print(f"Stai cercando: {nome_utente}")
+       
+        response = get(f"{BASE_URL}/{nome_utente}")
+
+        if response.status_code == 404:
+                print("Il profilo non esiste")
+            
+        else: 
+                print(f"Profilo {nome_utente} trovato")
+                controller = True 
+                break
+
+     except Exception as e:
+            print(f"Qualcosa è andato storto. Messaggio:{e}")
+            
+        
+    while controller:
+        counter +=1
+        url = f"{BASE_URL}/{nome_utente}?page={counter}&{END_URL}"
+        try:
+            response = get(url)
+            print(f"Errore: {response.status_code}")
+        
+
+            with open(f"tmp/pagina-{counter}.txt", "w") as f:
+        
+                f.write(response.text)
+                controller = is_next_button_present(response.text)
+                print("File salvato")
 
 
-    except Exception as e:
-      print(f"Errore: {e}") 
+        except Exception as e:
+            print(f"Errore: {e}") 
+    lista_utenti: list[str] = []
 
-  print("fine while perché i dati sono stati tutti scaricati")
+    for i in range(counter):
+        print(f"Couter: {i+1}")
+        with open(f"tmp/pagina-{i+1}.txt", "r") as f:
+            text = f.read()
+            lista_utenti.extend(re.findall(PATTERN_USER, text))
+
+        print(lista_utenti)
+
+        print("Fine programma.\nArrivederci")
  
 if __name__ == "__main__":
   main()
